41b84baff47c3c283a09e94d857195a010da1e9b
turinglabsorg <turinglabs@criptext.com>
2020-07-22 15:30:18 +0200

    unconfirmed transactions

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -8,6 +8,26 @@
       <div style="font-size:18px; margin-top:-15px"><br>{{ wallet.address }}</div>
     </h1>
     <apexchart v-if="!isLoading" height="300px" width="100%" type="line" :options="options" :series="series"></apexchart>
+    <div v-if="unconfirmed.length > 0">{{ configs.locales.home.unconfirmed_transactions }}</div>
+    <b-table v-if="unconfirmed.length > 0" :data="unconfirmed" style="font-size:14px">
+      <template slot-scope="props">
+          <b-table-column field="type" label="Type">
+            <b-icon icon="call-received" v-if="props.row.from !== wallet.address" style="color:green"></b-icon>
+            <b-icon icon="call-made" v-if="props.row.from === wallet.address" style="color:red"></b-icon>
+          </b-table-column>
+          <b-table-column field="from" label="From">
+            <v-gravatar style="float:left; border-radius:2px; margin-right: 10px; height:20px" :email="props.row.from" />
+            {{ props.row.from }}
+          </b-table-column>
+          <b-table-column field="to" label="To">
+            <v-gravatar style="float:left; border-radius:2px; margin-right: 10px; height:20px" :email="props.row.to" />
+            {{ props.row.to }}
+          </b-table-column>
+          <b-table-column field="value" label="Value">
+              {{ props.row.value.toFixed(8) }} LYRA
+          </b-table-column>
+      </template>
+    </b-table>
     <b-table :data="confirmed" :pagination-simple="true" :paginated="true" :per-page="5" style="font-size:14px">
       <template slot-scope="props">
           <b-table-column field="type" label="Type">
@@ -85,71 +105,98 @@
       app.wallet = await User.auth()
       if(app.wallet !== false){
         app.configs = await User.configs()
-        let balance = await app.scrypta.get('/balance/' + app.wallet.address)
-        app.balance = balance.balance
-
-        app.transactions = await app.scrypta.get('/transactions/' + app.wallet.address)
-        let transactions = app.transactions.data.reverse()
-        let initDate
-        let endDate
-        let last = transactions.length - 1
-
-        for(let x in transactions){
-          let transaction = app.transactions.data[x]
-          let date = app.getDate(transaction.time * 1000)
-          if(initDate === undefined){
-            initDate = date
-          }
-          if(parseFloat(x) === parseFloat(last)){
-            endDate = date
-          }
-          if(app.movements[date] === undefined){
-            app.movements[date] = 0
-          }
-          app.movements[date] += transaction.value
-        }
-
-        var getDaysArray = function(s,e) {for(var a=[],d=new Date(s);d<=e;d.setDate(d.getDate()+1)){ a.push(app.getDate(d));}return a;};
-        var daylist = getDaysArray(new Date(initDate),new Date(endDate))
-
-        for(let y in daylist){
-          let day = daylist[y]
-          let timestamp = new Date(day).getTime()
-          let prev = new Date(daylist[(y-1)]).getTime()
-
-          let daybalance = 0
-          let previous = app.graph[prev]
-          if(previous !== undefined){
-            daybalance = previous
-          }
-          if(app.movements[day] !== undefined){
-            daybalance += app.movements[day]
-          }
-          app.graph[timestamp] = parseFloat(daybalance.toFixed(8))
-        }
-
-        for(let time in app.graph){
-          app.options.xaxis.categories.push(parseInt(time))
-          app.series[0].data.push(app.graph[time])
-        }
-        app.transactions = app.transactions.data.reverse()
-        for(let x in app.transactions){
-          let transaction = app.transactions[x]
-          if(transaction.to[1] !== undefined || transaction.from[0] !== transaction.to[1]){
-            transaction.date = app.getDate(transaction.time*1000)
-            let expdate = transaction.date.split('-')
-            transaction.date = expdate[2] + '/' + expdate[1] + '/' + expdate[0] + ' at ' + app.getTime(transaction.time*1000)
-            transaction.from = transaction.from[0]
-            if(transaction.to[1] !== undefined){
-              transaction.to = transaction.to[1]
-            }
-            app.confirmed.push(transaction)
-          }
-        }
+        await app.fetchTransactions()
         app.isLoading = false
+        setInterval(function(){
+          app.fetchTransactions()
+        }, 15000)
       }
     },
     methods: {
+      fetchTransactions(){
+        const app = this
+        return new Promise(async response => {
+          let balance = await app.scrypta.get('/balance/' + app.wallet.address)
+          app.balance = balance.balance
+          app.options.xaxis.categories = []
+          app.series[0].data = []
+          app.transactions = await app.scrypta.get('/transactions/' + app.wallet.address)
+          let transactions = app.transactions.data.reverse()
+          let initDate
+          let endDate
+          let last = transactions.length - 1
+
+          for(let x in transactions){
+            let transaction = app.transactions.data[x]
+            let date = app.getDate(transaction.time * 1000)
+            if(initDate === undefined){
+              initDate = date
+            }
+            if(parseFloat(x) === parseFloat(last)){
+              endDate = date
+            }
+            if(app.movements[date] === undefined){
+              app.movements[date] = 0
+            }
+            app.movements[date] += transaction.value
+          }
+
+          var getDaysArray = function(s,e) {for(var a=[],d=new Date(s);d<=e;d.setDate(d.getDate()+1)){ a.push(app.getDate(d));}return a;};
+          var daylist = getDaysArray(new Date(initDate),new Date(endDate))
+
+          for(let y in daylist){
+            let day = daylist[y]
+            let timestamp = new Date(day).getTime()
+            let prev = new Date(daylist[(y-1)]).getTime()
+
+            let daybalance = 0
+            let previous = app.graph[prev]
+            if(previous !== undefined){
+              daybalance = previous
+            }
+            if(app.movements[day] !== undefined){
+              daybalance += app.movements[day]
+            }
+            app.graph[timestamp] = parseFloat(daybalance.toFixed(8))
+          }
+
+          for(let time in app.graph){
+            app.options.xaxis.categories.push(parseInt(time))
+            app.series[0].data.push(app.graph[time])
+          }
+          if(app.transactions.unconfirmed.length > 0){
+            for(let x in app.transactions.unconfirmed){
+              let transaction = app.transactions.unconfirmed[x]
+              if(transaction.to[1] !== undefined || transaction.from[0] !== transaction.to[1]){
+                transaction.date = app.getDate(transaction.time*1000)
+                let expdate = transaction.date.split('-')
+                transaction.date = expdate[2] + '/' + expdate[1] + '/' + expdate[0] + ' at ' + app.getTime(transaction.time*1000)
+                transaction.from = transaction.from[0]
+                if(transaction.to[1] !== undefined){
+                  transaction.to = transaction.to[1]
+                }
+                app.unconfirmed.push(transaction)
+              }
+            }
+          }
+
+          app.transactions = app.transactions.data.reverse()
+          for(let x in app.transactions){
+            let transaction = app.transactions[x]
+            if(transaction.to[1] !== undefined || transaction.from[0] !== transaction.to[1]){
+              transaction.date = app.getDate(transaction.time*1000)
+              let expdate = transaction.date.split('-')
+              transaction.date = expdate[2] + '/' + expdate[1] + '/' + expdate[0] + ' at ' + app.getTime(transaction.time*1000)
+              transaction.from = transaction.from[0]
+              if(transaction.to[1] !== undefined){
+                transaction.to = transaction.to[1]
+              }
+              app.confirmed.push(transaction)
+            }
+          }
+          response(true)
+        })
+      },
       getDate(time){
         let date = new Date(time)
         let y = date.getFullYear()
